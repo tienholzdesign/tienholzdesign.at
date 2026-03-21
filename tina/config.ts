31a57211c9b4dd8d20aee736b320898cdcf5e5b8
Focke Adrian <afocke@pm.me>
2026-03-21 10:19:48 +0100

    use main bnach

--- tina/config.ts
+++ tina/config.ts
@@ -1,26 +1,26 @@
-import { defineConfig } from "tinacms";
-import page from "./collections/page";
-import navigation from "./collections/navigation";
-import project from "./collections/project";
-import design from "./collections/design";
-import config from "./collections/config";
-import footer from "./collections/footer";
+import { defineConfig } from 'tinacms';
+import page from './collections/page';
+import navigation from './collections/navigation';
+import project from './collections/project';
+import design from './collections/design';
+import config from './collections/config';
+import footer from './collections/footer';
 
 const collections = [config, page, navigation, project, design, footer];
 
 export default defineConfig({
   clientId: process.env.NEXT_PUBLIC_TINA_CLIENT_ID,
-  branch: "main",
+  branch: 'main',
   token: process.env.TINA_TOKEN,
   media: {
     tina: {
-      publicFolder: "public",
-      mediaRoot: "uploads",
+      publicFolder: 'public',
+      mediaRoot: 'uploads',
     },
   },
   build: {
-    publicFolder: "public",
-    outputFolder: "admin",
+    publicFolder: 'public',
+    outputFolder: 'admin',
   },
   schema: {
     collections,
@@ -29,7 +29,7 @@ export default defineConfig({
     referenceDepth: 1,
   },
   cmsCallback: (cms) => {
-    cms.flags.set("branch-switcher", true);
+    cms.flags.set('branch-switcher', true);
     return cms;
   },
 });
