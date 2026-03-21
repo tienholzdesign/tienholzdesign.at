363f29c05b3991c72dbb661da53b43c8d63ed1ee
Focke Adrian <afocke@pm.me>
2026-03-21 09:58:32 +0100

    change components

--- components/CallToAction/CallToAction.tsx
+++ components/CallToAction/CallToAction.tsx
@@ -1,13 +1,18 @@
-import { Box, Card } from "@radix-ui/themes";
-import { tinaField } from "tinacms/dist/react";
-import type { PageBlocksCall_To_Action } from "../../tina/__generated__/types";
-import styles from "./CallToAction.module.css";
-import { themeConfig } from "../../config/theme-config";
-import { renderBlocks } from "../../tina/templating/utils";
+import { Box, Card } from '@radix-ui/themes';
+import { tinaField } from 'tinacms/dist/react';
+import type { PageBlocksCall_To_Action } from '../../tina/__generated__/types';
+import { themeConfig } from '../../config/theme-config';
+import { renderBlocks } from '../../tina/templating/utils';
 
 export default function Component(props: PageBlocksCall_To_Action) {
   const content = (
-    <Card className={styles.ctaBackground} data-tina-field={tinaField(props)}>
+    <Card
+      data-tina-field={tinaField(props)}
+      style={{
+        background:
+          'radial-gradient(circle, var(--color-background) 0%,var(--violet-10) 100%',
+      }}
+    >
       {props?.blocks?.map((block, index) => {
         return renderBlocks(block, index);
       })}
@@ -16,7 +21,7 @@ export default function Component(props: PageBlocksCall_To_Action) {
 
   return (
     <Box
-      mt={props.settings?.mt ?? "0"}
+      mt={props.settings?.mt ?? '0'}
       mb={props.settings?.mb ?? themeConfig.layout.padding}
     >
       {content}
--- components/Heading/Heading.tsx
+++ components/Heading/Heading.tsx
@@ -1,29 +1,29 @@
-import { Box, Heading } from "@radix-ui/themes";
-import type { PageBlocksHeading } from "../../tina/__generated__/types";
-import { useContext } from "react";
-import { LanguageContext } from "../../utils/context/language";
-import { tinaField } from "tinacms/dist/react";
-import { findIntlValue } from "../../tina/templating/special-fields";
-import { themeConfig } from "../../config/theme-config";
+import { Box, Heading } from '@radix-ui/themes';
+import type { PageBlocksHeading } from '../../tina/__generated__/types';
+import { useContext } from 'react';
+import { LanguageContext } from '../../utils/context/language';
+import { tinaField } from 'tinacms/dist/react';
+import { findIntlValue } from '../../tina/templating/special-fields';
+import { themeConfig } from '../../config/theme-config';
 
 export default function Component(props: PageBlocksHeading) {
   const language = useContext(LanguageContext);
-  const text = findIntlValue(language, "text");
+  const text = findIntlValue(language, 'text');
 
   const content = (
     <Heading
       // className="serif"
       data-tina-field={tinaField(props)}
       size={themeConfig.layout.headingSize}
-      style={{ whiteSpace: "pre-line" }}
+      style={{ whiteSpace: 'pre-line' }}
     >
-      {props[text] ? props[text] : "Add your text here"}
+      {props[text] ? props[text] : 'Add your text here'}
     </Heading>
   );
 
   return (
     <Box
-      mt={props.settings?.mt ?? "0"}
+      mt={props.settings?.mt ?? '0'}
       mb={props.settings?.mb ?? themeConfig.layout.padding}
     >
       {content}
