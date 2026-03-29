4ba503287a2d5e69b4972c38883305affa20a500
Focke Adrian <afocke@pm.me>
2026-03-29 10:03:15 +0200

    add unified sitemap for collections

--- app/stories/sitemap.ts
+++ app/stories/sitemap.ts
@@ -1,19 +1,5 @@
-import type { MetadataRoute } from "next";
-import client from "../../tina/__generated__/client";
-import { sanitizeFilenameForURL } from "../../tina/templating/validation";
-import { themeConfig } from "../../config/theme-config";
+import { generateCollectionSitemap } from '../../utils/generateCollectionSitemap';
 
-export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
-  const pages = (
-    await client.queries.projectConnection()
-  ).data.projectConnection.edges?.map((page) => page);
-
-  return pages!.map((page) => ({
-    url: `https://www.${themeConfig.project.domain}/projects/${sanitizeFilenameForURL(
-      page!.node!.name,
-    )}`,
-    lastModified: new Date(),
-    changeFrequency: "monthly",
-    priority: 0.5,
-  }));
+export default function sitemap() {
+  return generateCollectionSitemap('storyConnection', 'stories');
 }
